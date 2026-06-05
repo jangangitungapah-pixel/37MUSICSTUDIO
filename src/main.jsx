1e5b934c89f41f5410d45b011665e7e221609641
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-06-05 11:37:04 +0700

    Import sidebar motion fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './styles/fluent2-assets.css'
 import './styles/mobile-overhaul.css'
 import './styles/flat-minimal-system.css'
+import './styles/sidebar-motion-fix.css'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
