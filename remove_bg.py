66635ce356b5f622457938c71eb5dc51b8fb70ff
hazel <jangangitungapah@gmail.com>
2026-06-11 17:53:09 +0700

    reset: empty main for fresh rebuild

--- remove_bg.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from rembg import remove
-
-print("Loading high-res image...")
-input_img = Image.open('public/37upscale.png')
-print("Removing background with rembg...")
-output_img = remove(input_img)
-output_img.save('public/logo.png')
-print("Done! Replaced logo.png")
